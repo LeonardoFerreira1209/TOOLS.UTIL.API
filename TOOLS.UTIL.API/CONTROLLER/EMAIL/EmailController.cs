b377fc3767737d566e20219e5dcf4c39d5f2a814
Leonardo Almeida <Leo.Ferreira30@outlook.com>
2023-01-25 15:44:20 -0300

    Ajustes

--- TOOLS.UTIL.API/CONTROLLER/EMAIL/EmailController.cs
+++ TOOLS.UTIL.API/CONTROLLER/EMAIL/EmailController.cs
@@ -7,45 +7,44 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Serilog.Context;
 using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace TOOLS.UTIL.API.CONTROLLER.EMAIL
+namespace TOOLS.UTIL.API.CONTROLLER.EMAIL;
+
+[Route("api/[controller]")][ApiController]
+public class EmailController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmailController : ControllerBase
+    private readonly IEmailService _emailService;
+    private readonly ITemplateService _templateService;
+
+    public EmailController(IEmailService emailService, ITemplateService templateService)
     {
-        private readonly IEmailService _emailService;
-        private readonly ITemplateService _templateService;
+        _emailService = emailService;
+        _templateService = templateService;
+    }
 
-        public EmailController(IEmailService emailService, ITemplateService templateService)
+    /// <summary>
+    /// Método responsavel por fazer upload de um arquvivo html.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost("upload/template")][EnableCors("CorsPolicy")]
+    [SwaggerOperation(Summary = "Upload de arquivo.", Description = "Método responsavel por fazer um upload.")]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+    public async Task<ApiResponse<object>> UploadTemplate([Required] string name, [Required] string description, [Required] IFormFile formFile)
+    {
+        using (var reader = new StreamReader(formFile.OpenReadStream(), Encoding.UTF8))
         {
-            _emailService = emailService;
-            _templateService = templateService;
-        }
+            string fileContent = await reader.ReadToEndAsync();
 
-        /// <summary>
-        /// Método responsavel por fazer upload de um arquvivo html.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [HttpPost("upload/template")][EnableCors("CorsPolicy")]
-        [SwaggerOperation(Summary = "Upload de arquivo.", Description = "Método responsavel por fazer um upload.")]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
-        public async Task<ApiResponse<object>> UploadTemplate(string name, string description, IFormFile formFile)
-        {
-            using (var reader = new StreamReader(formFile.OpenReadStream(), Encoding.UTF8))
+            using (LogContext.PushProperty("Controller", "EmailController"))
+            using (LogContext.PushProperty("Payload", JsonConvert.SerializeObject(new List<string> { name, description, fileContent })))
+            using (LogContext.PushProperty("Metodo", "UploadTemplate"))
             {
-                string fileContent = await reader.ReadToEndAsync();
-
-                using (LogContext.PushProperty("Controller", "EmailController"))
-                using (LogContext.PushProperty("Payload", JsonConvert.SerializeObject(new List<string> { name, description, fileContent })))
-                using (LogContext.PushProperty("Metodo", "UploadTemplate"))
-                {
-                    return await Tracker.Time(() => _templateService.Save(name, description, fileContent), "Salvar template");
-                }
+                return await Tracker.Time(() => _templateService.Save(name, description, fileContent), "Salvar template");
             }
         }
     }
